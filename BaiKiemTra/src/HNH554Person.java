dc17b86a89abbeb8e69645710e1b322937d71a47
hoanghanh2004 <dubedi20cm@gmail.com>
2024-05-29 16:57:38 +0700

    lab 4

---
+++ BaiKiemTra/src/HNH554Person.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HNH554Person {
+    public static void main(String[] args) {
+        String ten;
+        int tuoi;
+        double chieucao;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nhap ten: ");
+        ten = sc.nextLine();
+        System.out.println("Nhap tuoi: ");
+        tuoi = sc.nextInt();
+        System.out.println("Nhap chieu cao (cm):");
+        chieucao = sc.nextDouble();
+
+        
+            ArrayList<String> names = new ArrayList<>();
+            System.out.println("So phan tu can them: ");
+            int n = sc.nextInt();
+            for (int i=0; i<n; i++){
+                System.out.println("Nhap so phan tu can them: ");
+                String name;
+                names.add(name);
+            }
+        }
+}
